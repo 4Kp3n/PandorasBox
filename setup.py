65ae390ff574154e5645f90120d95ecfc6faa719
4Kp3n <a.kellerer@proton.me>
2023-09-07 13:16:47 +0200

    Make dummy use absolute path

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import os
 
+script_path = os.path.dirname(os.path.abspath(__file__))
+tools = script_path + '/Tools'
+
 try:
-    os.makedirs('Tools')
+    os.makedirs(tools)
 except FileExistsError:
     print("Directory already exists")
 except Exception as e:
